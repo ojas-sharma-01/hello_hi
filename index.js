8f311ae45993f772002b8cfc29d9a79300cd15c4
Ojas_ <114296963+ojas-sharma-01@users.noreply.github.com>
2026-05-20 12:06:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/test", (req, res) => {
-  console.log(req.body);
+  console.log(JSON.stringify(req.body, null, 2));
   res.send("Received");
 });
 
