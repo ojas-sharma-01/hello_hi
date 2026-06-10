25756bae8c2c7e109a4d1951289d7ec7cc40cf34
Ojas_ <114296963+ojas-sharma-01@users.noreply.github.com>
2026-06-10 15:03:22 +0530

    Add /pay route to handle UPI payment redirection

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.post("/test", (req, res) => {
   res.send("Received");
 });
 
+app.get("/pay", (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  var upiLink = "upi://pay?pa=dominospizzaonline@ptybl&pn=JUBILANT%20FOODWORKS%20LIMITED&mc=5814&tid=PYTM60608804771253563397XXXXXXXXXXX&tr=60608804771253563397&am=280.35&cu=INR";
+  res.redirect(upiLink):
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server running");
 });
